072c921cb95976e84ec185b53dff85479080324f
jorleo021978 <162375672+jorleo021978@users.noreply.github.com>
2024-04-17 00:18:35 -0500

    Update claseUno.js
    quiete un comentario

--- jorge_tonguino_87453688/clase_4/claseUno.js
+++ jorge_tonguino_87453688/clase_4/claseUno.js
@@ -9,6 +9,6 @@ area.question('Ingresa la base del triángulo: ', (base) => {
     area.question('Ingresa la altura del triángulo: ', (altura) => {
         const areaTotal = (base * altura) / 2;
         console.log("El área del triángulo es:", areaTotal);
-        area.close(); // Cierra la interfaz de lectura de línea
+        area.close();
     });
 });
