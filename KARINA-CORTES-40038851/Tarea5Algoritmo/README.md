757fc0e3d3238083384098e3ed2eb24e9256e417
karycp <karinacortes1@gmail.com>
2024-04-18 15:26:28 -0500

    EjercicioDescuentos

---
+++ KARINA-CORTES-40038851/Tarea5Algoritmo/README.md
@@ -0,0 +1,4 @@
+Este es mi Ejercicio y para que funcione hay que modificar los valores de las variables
+mm me quedó grande recoger los datos por teclado usando la consola..
+seguire intentando!
+
---
+++ KARINA-CORTES-40038851/Tarea5Algoritmo/descuento.js
@@ -0,0 +1,35 @@
+const pesokilo=1;
+const precioKilo=1000;
+let total=0;
+let totalPagar=0;
+
+
+switch(true) {
+
+    case(pesokilo >2 && pesokilo<=5):
+        
+        total=precioKilo*pesokilo;
+        totalPagar = total-(total*10/100);
+        console.log(`Debe Cancelar, ${totalPagar}`);
+        break;
+    
+    case(pesokilo > 5 && pesokilo <=10):
+        total=precioKilo*pesokilo;
+        totalPagar = total-(total*15/100);
+        console.log(`Debe Cancelar, ${totalPagar}`);
+    break;
+
+    case(pesokilo > 10):
+        total=precioKilo*pesokilo;
+        totalPagar = total-(total*20/100);
+        console.log(`Debe Cancelar, ${totalPagar}`);
+        break;
+    
+    case(pesokilo > 0 && pesokilo <= 2):
+        console.log("NO Tiene Descuento");
+        break;
+    }
+
+
+
+
