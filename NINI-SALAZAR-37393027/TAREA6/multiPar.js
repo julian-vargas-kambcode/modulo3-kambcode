6f58c919c7c7cd238af5624cedfd910e18506a4a
IngNiniSalazar <johannaquitian140@gmail.com>
2024-04-23 22:54:51 -0500

    Tareas de la clase 6

--- NINI-SALAZAR-37393027/TAREA6/multiPar.js
+++ NINI-SALAZAR-37393027/TAREA6/multiPar.js
@@ -1,7 +1,19 @@
+/*## Ejercicio de Bucle For con Arrays:
+
+- Dado el siguiente array de números:
+
+  ```javascript
+  let numeros = [1, 2, 3, 4, 5];
+  ```
+
+- Utiliza un bucle for para multiplicar cada número por 2
+ y almacenar el resultado en un nuevo array. Luego, imprime el nuevo array.*/
+
+
 let numeros = [1, 2, 3, 4, 5];
 let newArray = [];
 
 for (let i = 0; i< numeros.length; i++) {
     newArray[i]=numeros[i]*2;
 }
-console.log(`MI NUEVO ARREGLO ES: ${newArray}`);
+console.log(`MI NUEVO ARREGLO MULTIPLICADO POR 2 ES: ${newArray}`);
