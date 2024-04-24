5f02fbb65bb2d88d4bb78a4c43feecbffc004341
DavidMontejoT <154643999+DavidMontejoT@users.noreply.github.com>
2024-04-23 21:57:10 -0500

    Add files via upload

---
+++ david-montejo-1016114213/tarejaclase6.js
@@ -0,0 +1,10 @@
+let numeros = [1, 2, 3, 4, 5];
+let resultados = [];
+
+// Utiliza un bucle for para multiplicar cada número por 2 y almacenar el resultado en un nuevo array
+for (let i = 0; i < numeros.length; i++) {
+    resultados.push(numeros[i] * 2);
+}
+
+// Imprime el nuevo array
+console.log("El nuevo array con los números multiplicados por 2 es:", resultados);
