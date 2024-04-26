dd695539590fd083aff10babb2b56ae8b402a58a
oswaldo-mendez <ing.oswaldomendez@gmail.com>
2024-04-25 23:53:57 -0500

    punto 2 listo

---
+++ oswaldo-mendez-1127060867/tarea8/punto2.js
@@ -0,0 +1,9 @@
+const estudiantes = [
+    { nombre: "Camilo", edad: 33, promedio: 10 },
+    { nombre: "Juan", edad: 30, promedio: 6 },
+    { nombre: "Andres", edad: 29, promedio: 8.7 },
+];
+
+console.log(estudiantes[1]);
+estudiantes[0].edad=25;
+console.log(estudiantes[0]);
