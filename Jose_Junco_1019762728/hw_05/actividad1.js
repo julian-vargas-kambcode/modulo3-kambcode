da26d7e7005b868d01b0e837f30460e7471afa6b
Cairon29 <josewjjunco@gmail.com>
2024-04-28 13:43:35 -0500

    arreglo de la tarea 5

--- Jose_Junco_1019762728/hw_05/actividad1.js
+++ Jose_Junco_1019762728/hw_05/actividad1.js
@@ -1,23 +1,18 @@
-/// first try
-
-const descuentoAplicado = (precio, peso) => {
-    let descuento = 0;
-    switch (peso){
-        case (peso > 2 && peso <= 5):
-            descuento = precio - (precio * 0.1);
-            console.log (descuento);
-            break;
-        case (peso > 5 && peso <= 10):
-            descuento = precio - (precio * 0.15);
-            console.log (descuento);
-            break;
-        case (peso > 10):
-            descuento = precio - (precio * 0.2);
-            console.log (descuento);
-            break;
-        default:
-            console.log (`no se aplica ningun descuento a tu compra de ${precio}`);
+const totalPagarAplicado = (precio, peso) => {
+    if(2 < peso <= 5){
+        let totalPagar = precio - (precio * 0.1);
+        console.log (totalPagar);
+    } else if (5 < peso <= 10){
+        let totalPagar = precio - (precio * 0.15);
+        console.log (totalPagar);
+    } else if ( peso > 10){
+        let totalPagar = precio - (precio * 0.2);
+        console.log (totalPagar);
+    } else{
+        console.log(`tienes que pagar sin descuento`)
     }
 }
 
-descuentoAplicado();
+totalPagarAplicado(10000, 7);
+
+
