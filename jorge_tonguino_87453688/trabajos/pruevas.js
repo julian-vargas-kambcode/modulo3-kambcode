bc85d0db01b5765f50f357ca0e79e7f3ebb0bd7f
jorleo021978 <jorleo021978@gmail.com>
2024-04-29 22:58:54 -0500

    actualizacion

--- jorge_tonguino_87453688/trabajos/pruevas.js
+++ jorge_tonguino_87453688/trabajos/pruevas.js
@@ -1,15 +1,9 @@
-function filtrarNumerosPares(array, callback) {
-  // Utiliza el método filter para filtrar los números pares del array
-  const numerosPares = array.filter(numero => numero % 2 === 0);
-  // Llama al callback con el array de números pares como argumento
-  callback(numerosPares);
+function filtrarArreglo(arreglo, callbackFiltro) {
+  return arreglo.filter(callbackFiltro);
 }
 
-function mostrarPares(numerosPares) {
-  // Imprime los números pares en la consola
-  console.log("Números pares:", numerosPares.join(', '));
-}
-
-// Ejemplo de uso:
-const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-filtrarPares(numeros, mostrarPares);
+const numeros = [1, 2, 3, 4, 5, 10, 15, 16];
+const numerosPares = filtrarArreglo(numeros, function (numero) {
+  return numero % 2 === 0;
+});
+console.log( "Este filtro me debuelve un nuevo arreglo con numeros pares: ",numerosPares); // Debería imprimir [2, 4]
