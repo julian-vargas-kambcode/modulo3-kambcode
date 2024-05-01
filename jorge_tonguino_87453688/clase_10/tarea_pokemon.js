5ed29383075ca846f7780c7eef03db5782e31e8b
jorleo021978 <162375672+jorleo021978@users.noreply.github.com>
2024-04-30 19:19:32 -0500

    Update tarea_pokemon.js

--- jorge_tonguino_87453688/clase_10/tarea_pokemon.js
+++ jorge_tonguino_87453688/clase_10/tarea_pokemon.js
@@ -26,18 +26,21 @@ async function main() {
     const primeros20Pokemons = await obtenerPrimeros20Pokemons();
     const pokemonsActualizados = [];
 
+    console.log("Nombres de los primeros 20 Pokémon:");
     for (const pokemon of primeros20Pokemons) {
+      console.log(pokemon.name); // Imprimir el nombre de cada Pokémon
       const nombrePokemon = pokemon.name;
       const informacionPokemon = await obtenerInformacionPokemon(nombrePokemon);
 
       if (informacionPokemon) {
-        pokemonsActualizados.push({ name: nombrePokemon, ...informacionPokemon });
+        pokemonsActualizados.push(informacionPokemon);
       }
     }
 
+    console.log("\nInformación completa de los primeros 20 Pokémon:");
     console.log(pokemonsActualizados);
   } catch (error) {
-    console.error("Error al obtener y actualizar la información de los primeros 20 Pokémon:", error);
+    console.error("Error al obtener y mostrar la información de los primeros 20 Pokémon:", error);
   }
 }
 
