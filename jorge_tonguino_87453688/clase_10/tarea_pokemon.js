fe342fea308a02ff1274b058aa31206fe941da66
jorleo021978 <jorleo021978@gmail.com>
2024-04-30 19:03:31 -0500

    tarea de clase 10

---
+++ jorge_tonguino_87453688/clase_10/tarea_pokemon.js
@@ -0,0 +1,45 @@
+const axios = require("axios");
+
+async function obtenerInformacionPokemon(nombrePokemon) {
+  try {
+    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${nombrePokemon}`);
+    return response.data;
+  } catch (error) {
+    console.error(`Error al obtener la información del Pokémon ${nombrePokemon}:`, error);
+    return null;
+  }
+}
+
+async function obtenerPrimeros20Pokemons() {
+  try {
+    const response = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=20");
+    const pokemons = response.data.results;
+    return pokemons;
+  } catch (error) {
+    console.error("Error al obtener la información de los primeros 20 Pokémon:", error);
+    return [];
+  }
+}
+
+async function main() {
+  try {
+    const primeros20Pokemons = await obtenerPrimeros20Pokemons();
+    const pokemonsActualizados = [];
+
+    for (const pokemon of primeros20Pokemons) {
+      const nombrePokemon = pokemon.name;
+      const informacionPokemon = await obtenerInformacionPokemon(nombrePokemon);
+
+      if (informacionPokemon) {
+        // Actualizar la información del Pokémon con la información obtenida
+        pokemonsActualizados.push({ name: nombrePokemon, ...informacionPokemon });
+      }
+    }
+
+    console.log(pokemonsActualizados);
+  } catch (error) {
+    console.error("Error al obtener y actualizar la información de los primeros 20 Pokémon:", error);
+  }
+}
+
+main();
