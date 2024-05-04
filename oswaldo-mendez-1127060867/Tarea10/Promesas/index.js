a1f181fbde452a39c4d867184e0312598064e726
oswaldo-mendez <ing.oswaldomendez@gmail.com>
2024-05-03 19:43:02 -0500

    tarea10

---
+++ oswaldo-mendez-1127060867/Tarea10/Promesas/index.js
@@ -0,0 +1,31 @@
+const axios  = require("axios");
+
+const API = "https://pokeapi.co/api/v2/pokemon";
+const pokemons = [];
+
+function main() {
+  axios
+    .get(API)
+    .then((response) => {
+      pokemons.push(...response.data.results);
+      //console.log(pokemons);
+      const promesas = pokemons.map((pokemon) => {
+        return axios.get(`${API}/${pokemon.name}`);
+      });
+
+      Promise.all(promesas).then((responses) => {
+        
+        const pokemonsSkills = pokemons.map((pokemon) => {
+          const response = responses.find((response) => response.data.name === pokemon.name); //con esto se llaman los datos de todos los pokemons y se guardan
+          return { ...pokemon, ...response.data };
+        });
+
+        console.log(pokemonsSkills);
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+ main();
